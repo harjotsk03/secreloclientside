355221d3792d147f8f5685487da557237a1df48d
Harjot Singh <harjotsk03@gmail.com>
2025-10-28 00:01:59 -0700

    fix small import issue

--- pages/app/repos/createSecrets/[id].js
+++ pages/app/repos/createSecrets/[id].js
@@ -1,12 +1,12 @@
 import { useRouter } from "next/navigation";
 
 export default function CreateSecrets() {
-  const router = useRouter();
-  const { showAlert } = useContext(AlertContext);
-  const { id } = router.query;
-  return(
+  // const router = useRouter();
+  // const { showAlert } = useContext(AlertContext);
+  // const { id } = router.query;
+  return (
     <div>
-        <h1>Multi Create</h1>
+      <h1>Multi Create</h1>
     </div>
-  )
+  );
 }
